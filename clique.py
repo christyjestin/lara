200bf07681223c3ca0b0e586bd9b77c0dcec7b1d
Christy Jestin <cjj9000@gmail.com>
2025-04-08 23:49:59 -0400

    Implement factor cliques

---
+++ clique.py
@@ -0,0 +1,48 @@
+from typing import List
+from abc import ABC, abstractmethod
+import torch
+
+class Clique(ABC):
+    """A general interface for representing subsets (or cliques) defined by some common rule e.g. all words that end in blank"""
+
+    @abstractmethod
+    def encode_sample(self, sample) -> torch.Tensor:
+        """Return the sample encoded as a PyTorch tensor"""
+        pass
+
+    @abstractmethod
+    def generate_example(self):
+        """Returns a random example based on the clique's rules"""
+        pass
+
+    @abstractmethod
+    def generate_sample(self):
+        """Returns a random sample from the clique's superset"""
+        pass
+
+    @abstractmethod
+    def __contains__(self, sample) -> bool:
+        """Return whether the input `sample` is included in this clique"""
+        pass
+
+    def generate_counterexample(self):
+        raise NotImplementedError
+
+    def generate_examples(self, n: int) -> List:
+        return [self.generate_example() for _ in range(n)]
+
+    def generate_counterexamples(self, n: int) -> List:
+        """Use the custom helper function if available; otherwise fall back to rejection sampling"""
+        try:
+            return [self.generate_counterexample() for _ in range(n)]
+        except NotImplementedError:
+            lst = []
+            while len(lst) < n:
+                sample = self.generate_sample()
+                if sample not in self: # add elements that aren't in the clique
+                    lst.append(sample)
+            return lst
+
+    def encode_samples(self, samples: List) -> torch.Tensor:
+        """Encode a list of samples as a Tensor of size n x d where n is the length of the list"""
+        return torch.stack([self.encode_sample(sample) for sample in samples])
---
+++ factor_clique.py
@@ -0,0 +1,35 @@
+import random
+import torch
+
+from clique import Clique
+
+MAX_INT = 2 ** 31 - 1
+
+class FactorClique(Clique):
+    def __init__(self, factor, max_val=MAX_INT):
+        self.factor = factor
+        self.max_val = max_val
+
+    def generate_example(self) -> int:
+        return random.randint(1, self.max_val // self.factor) * self.factor
+
+    def generate_counterexample(self) -> int:
+        # subtract one to ensure val < max_val
+        val = random.randint(1, self.max_val // self.factor - 1) * self.factor
+        return val + random.randint(1, self.factor - 1)
+
+    def generate_sample(self) -> int:
+        return random.randint(1, self.max_val)
+
+    def encode_sample(self, n) -> torch.Tensor:
+        assert isinstance(n, int) and n > 0
+
+        e = torch.zeros(32)
+        binary = bin(n)[2:]
+        for i in range(1, len(binary) + 1):
+            e[-i] = int(binary[-i])
+        return e
+
+    def __contains__(self, sample: int) -> bool:
+        assert isinstance(sample, int) and sample > 0, f"Invalid sample: {sample}"
+        return sample % self.factor == 0
